b904716325618b054f8c249cf349004d8489d458
Andrew Cassell <alc277@gmail.com>
2016-05-16 23:26:27 -0400

    amount class for bets, all wagers and prizes in whole credits

---
+++ src/Currency/Amount.php
@@ -0,0 +1,58 @@
+<?php
+namespace Cassell\Casino\Currency;
+
+/**
+ * Class Amount
+ * @package Cassell\Casino\Currency
+ */
+class Amount
+{
+    /**
+     * @var int
+     */
+    private $intAmount;
+
+    /**
+     * Amount constructor.
+     * @param $intAmount
+     */
+    public function __construct($intAmount)
+    {
+        $this->intAmount = (int) $intAmount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAmount()
+    {
+        return $this->intAmount;
+    }
+
+    /**
+     * @param Amount $amount
+     * @return Amount
+     */
+    public function addAmount(Amount $amount)
+    {
+        return new Amount($this->getAmount() + $amount->getAmount());
+    }
+
+    /**
+     * @param Amount $amount
+     * @return Amount
+     */
+    public function reduceAmount(Amount $amount)
+    {
+        return new Amount($this->getAmount() - $amount->getAmount());
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPositive()
+    {
+        return $this->getAmount() > 0;
+    }
+
+}
---
+++ tests/Currency/AmountTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace Cassell\Test\Casino\Currency;
+
+use Cassell\Casino\Currency\Amount;
+
+class AmountTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function testZero()
+    {
+        $amt = new Amount(0);
+        $this->assertEquals(0,$amt->getAmount());
+    }
+
+    /**
+     * @test
+     */
+    public function testRound()
+    {
+        $amt = new Amount(5.45);
+        $this->assertEquals(5,$amt->getAmount());
+    }
+
+    /**
+     * @test
+     */
+    public function testIsPositive()
+    {
+        $this->assertTrue((new Amount(1))->isPositive());
+        $this->assertTrue((new Amount(5))->isPositive());
+        $this->assertFalse((new Amount(0))->isPositive());
+        $this->assertFalse((new Amount(-5))->isPositive());
+    }
+
+    /**
+     * @test
+     */
+    public function testAdd()
+    {
+        $this->assertEquals(5,(new Amount(1))->addAmount(new Amount(4))->getAmount());
+        $this->assertEquals(46,(new Amount(31))->addAmount(new Amount(15))->getAmount());
+    }
+
+    /**
+     * @test
+     */
+    public function testSub()
+    {
+        $this->assertEquals(-3,(new Amount(1))->reduceAmount(new Amount(4))->getAmount());
+        $this->assertEquals(16,(new Amount(31))->reduceAmount(new Amount(15))->getAmount());
+    }
+
+}
