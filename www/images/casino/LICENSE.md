f439126bf90e51ab9bcd5b156be289d66ddb1c85
Andrew Cassell <alc277@gmail.com>
2016-05-19 00:17:29 -0400

    aframe scene, camera, assets. show current balance at bottom of view

---
+++ www/images/casino/LICENSE.md
@@ -0,0 +1,24 @@
+
+
+https://www.flickr.com/photos/heiwa4126/3459395268/in/photolist-5feB9H-arzYXZ-5vr42w-33FzgJ-6gGiG1-GJ6wNS-GdVCXc-GJ6wA7-H3fBEv-9jvPQQ-5vmGc2-5vmH7K-5vr3nw-6U3cSm-JpuNx-76VcdL-76vdKB-7rSK6o-79we9E-521TTJ-5mjNPP
+https://creativecommons.org/licenses/by/2.0/
+
+
+Tokyo Disnyland has a giant shopping mall for shopping crazies. And every shopping mall has a cineplex. This is that.
+It's indoor. Sky is panted dome.
+ 
+Please enjoy the interactive viewer! (thanks to fieldOfView and Aldo)
+And small but quick interactive viewer is here (Wrapr Beta)
+ 
+another interactive version:
+www.360cities.net/image/cinema-ikspiari-japan
+(need Adobe Flash player)
+ 
+- SLR camera and lens: Nikon D80 /w Sigma 8mm fisheye
+- handheld (with Simon's "HaPaLa3")
+- 4 pan (Philopod pitch variation [datails]) 3EX(2EV) each.
+- software: ptgui, Photoshop and enfuse on MS-Windows XP
+ 
+See where this picture was taken. [?]
+[MAP by ALPSLAB]
+ 
--- www/js/scene.js
+++ www/js/scene.js
@@ -1,9 +1,106 @@
 $(document).ready(function () {
 
-    $.getJSON('api/balance.php',function(resp) {
-
-        console.log(resp);
-
+    var Assets = React.createClass({
+        render: function () {
+            return <a-assets>
+                      <img id="casino" src="./images/casino/cinema-ikspiari-japan.jpg"/>
+                   </a-assets>;
+        }
     });
+
+    var Sky = React.createClass({
+        render: function () {
+            return React.createElement('a-sky', {'src': '#casino', 'rotation': '0 90 0'});
+        }
+    });
+
+    var Camera = React.createClass({
+        render: function () {
+            return <a-entity id="camera" camera position="0 2.5 7.5" look-controls="enabled: true"
+                             cursor-visible="enabled: false" wasd-controls="enabled: true">
+                       <a-cursor color="#ffffff"/>
+                   </a-entity>;
+        }
+    });
+
+    var Balance = React.createClass({
+
+        //updateBalance: function(newBalance) {
+        //    this.setState({
+        //        balance: newBalance
+        //    });
+        //},
+        //getInitialState: function () {
+        //    return {
+        //        balance: 0
+        //    };
+        //},
+        render: function () {
+            return React.createElement('a-entity',{
+                text: "text: Balance: " + this.props.balance + "; size: 0.3",
+                material: "color: #AAA",
+                position: "-1 -3 0",
+                rotation: "-45 0 0"
+            });
+        }
+    });
+
+    var Casino = React.createClass({
+
+        componentDidMount: function() {
+            $.ajax({
+                url: this.props.balanceUrl,
+                dataType: 'json',
+                cache: false,
+                success: function(resp) {
+                    this.updateBalance(resp.balance);
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    console.error(this.props.url, status, err.toString());
+                }.bind(this)
+            });
+        },
+
+        updateBalance: function (newBalance) {
+            this.setState({
+                balance: newBalance
+            });
+            //this.refs.balance.updateBalance(newBalance);
+        },
+
+        getInitialState: function () {
+            return { balance: 0};
+        },
+
+        render: function () {
+            return (
+                React.createElement('a-scene', {}, [
+                    <Assets key="assets"/>,
+                    <Sky key="sky"/>,
+                    <Camera key="camera"/>,
+                    <Balance ref="balance" balance={this.state.balance} key="balance" />
+                ]));
+        }
+    });
+
+    var casinoInstance = ReactDOM.render(<Casino balanceUrl="api/balance.php"/>, $("#scene-container").get(0));
+
+
+    function run () {
+
+
+    }
+
+    var scene = $('a-scene').get(0);
+
+    if (scene.hasLoaded) {
+        run();
+    } else {
+        scene.addEventListener('loaded', run);
+    }
+
+
+
+
 
 });
