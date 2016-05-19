2f2d833577eab41e15beecc9caf8aaf122467076
Andrew Cassell <alc277@gmail.com>
2016-05-18 23:31:13 -0400

    wager

---
+++ src/Currency/Wager.php
@@ -0,0 +1,11 @@
+<?php
+namespace Cassell\Casino\Currency;
+
+class Wager extends Amount
+{
+    public function __construct($amount)
+    {
+        parent::__construct($amount);
+    }
+
+}
---
+++ tests/Currency/WagerTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Cassell\Test\Casino\Currency;
+
+use Cassell\Casino\Currency\Amount;
+use Cassell\Casino\Currency\Wager;
+
+class WagerTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function testInstanceOfAmount()
+    {
+        $this->assertInstanceOf(Amount::class,new Wager(1));
+    }
+
+    /**
+     * @test
+     */
+    public function testPositiveAmount()
+    {
+        $this->assertEquals(1,(new Wager(1))->getAmount());
+        $this->assertEquals(15,(new Wager(15))->getAmount());
+    }
+
+
+}
