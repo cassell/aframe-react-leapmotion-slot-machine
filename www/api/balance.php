b1cb5c95feb1b7ffa254acccd77fae9323c16ca6
Andrew Cassell <alc277@gmail.com>
2016-05-18 23:58:34 -0400

    minor edit: cs fixer

--- www/api/balance.php
+++ www/api/balance.php
@@ -8,8 +8,8 @@ require_once __DIR__ . "/../../vendor/autoload.php";
 $session = new Session();
 $session->start();
 
-if(! $session->has('balance')) {
-    $session->set('balance',new Balance(100));
+if (!$session->has('balance')) {
+    $session->set('balance', new Balance(100));
 }
 
 $json = new \Symfony\Component\HttpFoundation\JsonResponse(['balance' => $session->get('balance')->getAmount()]);
